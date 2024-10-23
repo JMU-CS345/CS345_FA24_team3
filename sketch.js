3fdf8432641bcb81fdeb7d0add2b4dbde941a495
Nicholas <152906156+Drake3Ne@users.noreply.github.com>
2024-10-23 09:16:49 -0400

    Player and background Assets

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+let numFramesY = 12;
+let standing = 1;
+let walking = 2;
+let jumping = 3;
+let frameWidth = 24
+let frameHeight = 24;
+var player = { x: 10, y: 0, w: 150, h: 150, v: 0, a: 1 }
+function preload() {
+  playerImage = loadImage("assets/Character.png");
+  level1 = loadImage("assets/level1.png");
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  frameRate(60);
+  player.y = windowHeight - player.h;
 }
 
 function draw() {
-  background(220);
+  background(level1);
+  player.v = player.v + player.a
+  player.y = player.y + player.v;
+  if (player.y + player.h >= windowHeight) {
+    player.y = windowHeight - player.h;
+  }
+  image(playerImage, player.x, player.y, player.w, player.h, 0, 0, frameWidth, frameHeight);
 }
