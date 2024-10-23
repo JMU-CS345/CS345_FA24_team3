850c86ad34d7fd66acef4bc5d2755c2732e57537
Nicholas <152906156+Drake3Ne@users.noreply.github.com>
2024-10-23 09:30:45 -0400

    player update

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ let jumping = 3;
 let frameWidth = 24
 let frameHeight = 24;
 var player = { x: 10, y: 0, w: 150, h: 150, v: 0, a: 1 }
+
 function preload() {
   playerImage = loadImage("assets/Character.png");
   level1 = loadImage("assets/level1.png");
@@ -24,15 +25,18 @@ function draw() {
   if (player.y + player.h >= windowHeight) {
     player.y = windowHeight - player.h;
   }
-  image(playerImage, player.x, player.y, player.w, player.h, 0, 0, frameWidth, frameHeight);
-}
 
-function playerMovement() {
-  if (keyIsDown(RIGHT_ARROW)) {
-    player.x++;
-  }
-  if (keyIsDown(LEFT_ARROW)) {
-    player.x--;
+  function playerMovement() {
+    if (keyIsDown(RIGHT_ARROW)) {
+      player.x++;
+      image(playerImage, player.x, player.y, player.w, player.h, 100, 100, frameWidth, frameHeight);
+    }
+    else if (keyIsDown(LEFT_ARROW)) {
+      player.x--;
+    }
+    else {
+      image(playerImage, player.x, player.y, player.w, player.h, 0, 0, frameWidth, frameHeight);
+    }
   }
 }
 function keyPressed() {
